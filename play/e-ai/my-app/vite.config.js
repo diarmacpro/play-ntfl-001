6f04a7891281b3d32d168be7e9ff27a5ba03ed77
diarskymax <diarskymax@gmail.com>
2025-07-02 11:18:10 +0700

    005.v

---
+++ play/e-ai/my-app/vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // ubah ini jika Netlify pakai subfolder
+})
